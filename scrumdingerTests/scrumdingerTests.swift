5037d80cf146dd7ba8aa2989f073f535ed272af9
Dmitry Chubarov <dchubarov@devexperts.com>
2025-02-18 18:35:24 +0400

    Initial Commit

---
+++ scrumdingerTests/scrumdingerTests.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerTests.swift
+//  scrumdingerTests
+//
+//  Created by Dmitry Chubarov on 17.02.25.
+//
+
+import Testing
+@testable import scrumdinger
+
+struct scrumdingerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
